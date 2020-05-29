c88aed05b6752e451f64512bbd9421f891c0e3a6
longdh92 <54471003+longdh92@users.noreply.github.com>
2020-05-29 17:11:57 +0700

    sửa linh tinh

--- src/main/java/com/vn/controller/AccountController.java
+++ src/main/java/com/vn/controller/AccountController.java
@@ -1,13 +1,7 @@
 package com.vn.controller;
 
-import com.vn.model.Account;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class AccountController {
@@ -16,6 +10,5 @@ public class AccountController {
     public String index() {
         return "index";
     }
-
 
 }
